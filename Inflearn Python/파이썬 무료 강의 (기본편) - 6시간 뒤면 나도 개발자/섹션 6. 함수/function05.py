1e766a4761b746361c5542d820a44a79aadf9779
SunJooSong <mundalling@naver.com>
2023-05-20 16:16:08 +0900

    python 함수 공부

---
+++ Inflearn Python/파이썬 무료 강의 (기본편) - 6시간 뒤면 나도 개발자/섹션 6. 함수/function05.py
@@ -0,0 +1,18 @@
+# 지역변수와 전역변수
+gun = 10
+
+def checkpoint(soldiers): # 경계근무
+    # gun = 20 # 지역변수
+    global gun # 전역 공간에 있는 gun 사용 -> 일반적으로 권장되지는 않음
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    
+def checkpoint_ret(gun, soldiers):
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+    
+print("전체 총 : {0}".format(gun))
+# checkpoint(2) # 2명이 경계 근무 나감
+gun = checkpoint_ret(gun, 2)
+print("남은 총 : {0}".format(gun))
